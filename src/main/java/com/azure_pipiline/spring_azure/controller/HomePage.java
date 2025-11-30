fdbb6e396f4bcf44e6ecc9aa1b3b3e5e57cc8444
Srinu-rj <dnsrinu143@gmail.com>
2025-11-30 16:21:17 +0530

    java 17 updated

--- src/main/java/com/azure_pipiline/spring_azure/controller/HomePage.java
+++ src/main/java/com/azure_pipiline/spring_azure/controller/HomePage.java
@@ -8,18 +8,22 @@
 package com.azure_pipiline.spring_azure.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomePage {
+
     @GetMapping("/test")
     public String test(){
         return "Hello All";
     }
+
     @GetMapping("/wellcome")
     public String wellcome(){
         return "Wellcome to Azure Spring Boot Application";
     }
+
 
 }
--- src/main/java/com/azure_pipiline/spring_azure/controller/Webpage.java
+++ src/main/java/com/azure_pipiline/spring_azure/controller/Webpage.java
@@ -1,4 +1,16 @@
 package com.azure_pipiline.spring_azure.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/html")
 public class Webpage {
+
+    @GetMapping("/application")
+    public String demo_application(){
+        return "home";
+    }
+
 }
